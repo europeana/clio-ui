96547ed9cb968a547382c15b7a5a688005e7537b
andyjmaclean <andyjmaclean@gmail.com>
2025-12-02 12:48:47 +0100

    Build Set-Up

---
+++ src/assets/env.template.js
@@ -0,0 +1,5 @@
+(function (window) {
+  window.__env = window.__env || {};
+  const env = window.__env;
+  env.serverAPI = "${APP_SERVER_API}";
+})(this);
---
+++ src/environments/apisettings.ts
@@ -0,0 +1,9 @@
+const getEnvVar = (key: string): string | null => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const env = (window as any).__env;
+  return env ? env[key] : null;
+};
+
+export const apiSettings = {
+  serverAPI: getEnvVar('serverAPI') as string
+};
