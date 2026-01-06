d3ee3b18c9b4d6bc961fb957f88791e80959354f
andyjmaclean <andyjmaclean@gmail.com>
2026-01-06 18:36:42 +0100

    Setup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  public readonly keycloak = inject(Keycloak);
+
+}
